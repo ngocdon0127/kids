34965787b4e4f16bc3770342a7bf43d5c2f15b85
Ngoc Don <kaitoukid2701@gmail.com>
2016-02-19 11:31:24 +0700

    Fix nav login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Evangels English
                 </a>
             </div>
 
