043c4bdeba097112cb6780814160a14ef94e7927
Ngoc Don <kaitoukid2701@gmail.com>
2016-02-24 09:24:17 +0700

    Fix

--- resources/views/1.blade.php
+++ resources/views/1.blade.php
@@ -1,16 +1,12 @@
-@extends('layouts.main')
-@section('head.title')
-	Kids
-@endsection
-@section('body.content')
-	<div class="container">
-		<div id="divimg"><img id="img" src="/test.jpg" alt="" class="img-responsive" /></div>
-		<script>
-			function ob (x) {
-				return document.getElementById(x);
-			}
-			console.log(ob('divimg').clientHeight);
-		</script>
-	</div>
-
-@endsection
+<div class="container">
+	<script type="text/javascript" src="/js/jquery/jquery.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<div id="divimg"><img id="img" src="/test.jpg" alt="" class="img-responsive" /></div>
+	<script>
+		function ob (x) {
+			return document.getElementById(x);
+		}
+		console.log(ob('divimg').clientHeight);
+	</script>
+</div>
